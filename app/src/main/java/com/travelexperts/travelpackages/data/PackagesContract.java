84716cd32fef1c6ab083d5705b4b81e7f8091abb
Chris <christopher.billington.school@gmail.com>
2017-03-20 21:53:18 -0600

    modified PackagesContract.java -- added Uris for accessing resources

--- app/src/main/java/com/travelexperts/travelpackages/data/PackagesContract.java
+++ app/src/main/java/com/travelexperts/travelpackages/data/PackagesContract.java
@@ -1,6 +1,7 @@
 package com.travelexperts.travelpackages.data;
 
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,11 +11,25 @@ import android.provider.BaseColumns;
 public class PackagesContract {
 
     /**
+     * These 3 constants provide the base uri of <scheme>://<content-authority> to build all
+     * resource endpoints from.
+     * <scheme>://<content-authority>/<path>
+     *
+     */
+    public static final String CONTENT_AUTHORITY = "com.travelexperts.travelpackages";
+    public static final String SCHEME = "content://";
+    public static final Uri BASE_URI = Uri.parse(SCHEME + CONTENT_AUTHORITY);
+
+    // path to packages resource (db table)
+    public static final String PATH_PACKAGES = "packages";
+    /**
      *
      * inner class to represent the Travel Experts packages table.
      */
     public static final class PackagesEntry implements BaseColumns{
         public static final String TABLE_NAME = "packages";
+        public static final Uri CONTENT_URI = BASE_URI.buildUpon().appendPath(PATH_PACKAGES)
+                .build();
 
         public static final String COLUMN_PACKAGE_NAME = "PkgName";
         public static final String COLUMN_PACKAGE_START_DATE = "PkgStartDate";
