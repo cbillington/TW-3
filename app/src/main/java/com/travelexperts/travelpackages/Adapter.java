7c684b52560a0f3cad8feeb606a76ee40750c0bb
jermayne <j.willz21@hotmail.com>
2017-03-24 12:34:59 -0600

    added recycler view demo (just xml)

---
+++ app/src/main/java/com/travelexperts/travelpackages/Adapter.java
@@ -0,0 +1,47 @@
+package com.travelexperts.travelpackages;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.travelexperts.travelpackages.fragments.all_tab;
+
+/**
+ * Created by 744095 on 3/22/2017.
+ */
+
+public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+   Context context;
+   String[] items;
+    public Adapter(Context context, String[]items){
+        this.items=items;
+        this.context = context;
+    }
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View all = inflater.inflate(R.layout.all_tab, parent, false);
+        Item item = new Item(all);
+        return item;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((Item)holder).textView.setText(items[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.length;
+    }
+    public class Item extends RecyclerView.ViewHolder {
+        TextView textView;
+        public Item(View itemView) {
+            super(itemView);
+            textView= (TextView) itemView.findViewById(R.id.item);
+        }
+    }
+}
--- app/src/main/java/com/travelexperts/travelpackages/fragments/all_tab.java
+++ app/src/main/java/com/travelexperts/travelpackages/fragments/all_tab.java
@@ -2,22 +2,41 @@ package com.travelexperts.travelpackages.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.travelexperts.travelpackages.Adapter;
 import com.travelexperts.travelpackages.R;
+import com.travelexperts.travelpackages.data.PackagesDbHelper;
+
 
 /**
  * Created by 744095 on 3/20/2017.
  */
 
 public class all_tab extends Fragment {
+    private RecyclerView rvAllTab;
+    private RecyclerView.Adapter rvAdapter;
+    private RecyclerView.LayoutManager rvLayoutManager;
+    String[] items={"package 1", "package 2", "package 3"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.all_tab, container, false);
+
+        //recyclerview
+        rvAllTab = (RecyclerView) rootView.findViewById(R.id.rvAllTab);
+        rvAllTab.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvAllTab.setHasFixedSize(true);
+        rvAllTab.setAdapter(new Adapter(getContext(), items));
+
         return rootView;
     }
+
+
+
 }
